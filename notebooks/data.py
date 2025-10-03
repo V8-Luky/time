3583b72b29240bbaadcb7b507b2f86d7d6c8cd14
Luca Kyburz <kyburzluca@gmail.com>
2025-10-03 16:12:35 +0200

    Exercise 2

--- notebooks/data.py
+++ notebooks/data.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import gc
 from typing import List, Iterable, Dict, Tuple
 
@@ -8,6 +9,7 @@ import eurostat
 import yfinance as yf
 from statsmodels.datasets import co2
 from scipy.datasets import electrocardiogram
+from tqdm.autonotebook import tqdm
 
 
 def get_electrocardiogram() -> pd.DataFrame:
@@ -145,3 +147,28 @@ def get_periodic_process(seed: int, npoints: int = 100, sd: float = 1., period: 
     name = f'Periodic process ($a={a:.2f}, b={b:.2f}, \omega=1/{period}$)'
     x_t = a * c_t + b * s_t
     return x_t, name
+
+
+def compact_to_expanded(
+    df, timeseries_col, static_cols, time_varying_cols, ts_identifier
+):
+    def preprocess_expanded(x):
+        ### Fill missing dates with NaN ###
+        # Create a date range from  start
+        dr = pd.date_range(
+            start=x["start_timestamp"],
+            periods=len(x["energy_consumption"]),
+            freq=x["frequency"],
+        )
+        df_columns = defaultdict(list)
+        df_columns["timestamp"] = dr
+        for col in [ts_identifier, timeseries_col] + static_cols + time_varying_cols:
+            df_columns[col] = x[col]
+        return pd.DataFrame(df_columns)
+
+    all_series = []
+    for i in tqdm(range(len(df))):
+        all_series.append(preprocess_expanded(df.iloc[i]))
+    df = pd.concat(all_series)
+    del all_series
+    return df
